3b6a7c62d896a3e429dfb00b45851f32f6afe768
Orlando dos Santos Dourado <d708819@rede.sp>
2025-06-14 09:50:47 -0300

    segundo commit
    inserindo mais um codigo

---
+++ Desafios em Python/canatenando_dados.py
@@ -0,0 +1,7 @@
+info1 = input ("Digite o primeiro dado: ")
+info2 = input ("Digite o segundo dado: ")
+
+# Concatenando os dados
+dados_concatenados = info1 + " " + info2
+print("Dados concatenados:", dados_concatenados)
+# Exemplo de uso
